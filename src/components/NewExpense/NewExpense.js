23f88f80a5467564a10b27e816cf42eb309734ff
Fahad <alfahd360@gmail.com>
2023-10-11 14:45:18 +0100

    changes for the graph addition

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [toggleForm, setToggleForm] = useState(false);
   const saveExpenseDataHandler = (enterdExpenseData) => {
     const expenseDate = {
       ...enterdExpenseData,
@@ -10,11 +11,28 @@ const NewExpense = (props) => {
     };
     console.log(expenseDate);
     props.onAddExpense(expenseDate);
+    setToggleForm(false);
+  };
+  const clickHandler = () => {
+    setToggleForm(true);
+  };
+
+  const cancelClickHandler = () => {
+    setToggleForm(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseDate={saveExpenseDataHandler} />
+      {toggleForm ? (
+        <ExpenseForm
+          onSaveExpenseDate={saveExpenseDataHandler}
+          cancelAction={cancelClickHandler}
+        />
+      ) : (
+        <div className="new-expenses__actions">
+          <button onClick={clickHandler}>Add new Expense</button>
+        </div>
+      )}
     </div>
   );
 };
